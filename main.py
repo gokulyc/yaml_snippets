8dbde512a01541c53f37f70df06551a06325f092
Gokul Y <gokulyc@gmail.com>
2024-01-22 18:08:01 +0530

    Added debug flag | Added new yaml snippets

--- main.py
+++ main.py
@@ -4,12 +4,19 @@ import pathlib
 from rich import print as rprint
 import os
 from yaml.composer import ComposerError
+from dotenv import load_dotenv
+
+rprint(f"Is .env loaded : {load_dotenv()}")
+
+IS_DEBUG = True if os.getenv("DEBUG") == "True" else False
+
+# rprint(f"IS_DEBUG : {IS_DEBUG=} {type(IS_DEBUG)}")
 
 ROOT_YAML_DIR = pathlib.Path("yaml_snips").resolve()
 ROOT_JSON_OUT_DIR = pathlib.Path("json_out").resolve()
 
 
-def generate_json_from_yaml(yaml_file_path: os.PathLike, json_out_dir: os.PathLike):
+def generate_json_from_yaml(yaml_file_path: pathlib.Path, json_out_dir: pathlib.Path):
     json_file_name = f"{yaml_file_path.stem}.json"
     try:
         out = yaml.safe_load(yaml_file_path.read_text())
@@ -18,6 +25,7 @@ def generate_json_from_yaml(yaml_file_path: os.PathLike, json_out_dir: os.PathLi
             (json_out_dir / json_file_name).open(mode="w"),
             default=str,
         )
+        rprint(f"Wrote {json_file_name} !")
     except ComposerError:
         rprint("Multi docs found in stream...")
         out = yaml.safe_load_all(yaml_file_path.read_text())
@@ -28,22 +36,29 @@ def generate_json_from_yaml(yaml_file_path: os.PathLike, json_out_dir: os.PathLi
                 (json_out_dir / json_file_name).open(mode="w"),
                 default=str,
             )
+            rprint(f"Wrote {json_file_name} !!!")
+
+
+def rprint_yaml_to_json(yaml_file_path: pathlib.Path):
+    try:
+        out = yaml.safe_load(yaml_file_path.read_text())
+        rprint(f"{yaml_file_path.stem}")
+        rprint(out)
+        rprint("--------------")
+    except ComposerError as e:
+        rprint("Multi docs found in stream...")
+        out = yaml.safe_load_all(yaml_file_path.read_text())
+        rprint(list(out))
+        rprint("--------------")
 
 
 if __name__ == "__main__":
-    file_gen = ROOT_YAML_DIR.rglob("*.yaml")
-    # rprint(list(file_gen))
-    for yaml_file_path in file_gen:
-        generate_json_from_yaml(yaml_file_path, ROOT_JSON_OUT_DIR)
-        rprint(f"Wrote {yaml_file_path.stem} !!!")
+    file_path_list = list(ROOT_YAML_DIR.rglob("*.yaml"))
+    rprint(f"File(s) found : {len(file_path_list)}")
 
-    # for yaml_file_path in file_gen:
-    #     try:
-    #         out = yaml.safe_load(yaml_file_path.read_text())
-    #         rprint(out)
-    #         rprint("--------------")
-    #     except ComposerError as e:
-    #         rprint("Multi docs found in stream...")
-    #         out = yaml.safe_load_all(yaml_file_path.read_text())
-    #         rprint(list(out))
-    #         rprint("--------------")
+    if IS_DEBUG:
+        for yaml_file_path in file_path_list:
+            rprint_yaml_to_json(yaml_file_path)
+    else:
+        for yaml_file_path in file_path_list:
+            generate_json_from_yaml(yaml_file_path, ROOT_JSON_OUT_DIR)
